6e864634f601340c004df12e79ccb782251aff16
diegopaff <difercuru@gmail.com>
2024-01-10 10:46:41 -0300

    style: basic ui finished

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,5 +1,20 @@
 function FeedbackForm() {
-  return <div>FeedbackForm</div>;
+  return (
+    <form className="form">
+      <textarea
+        id="feedback-textarea"
+        placeholder="Insert"
+        spellCheck={false}
+      />
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, rememeber to #hastag the company
+      </label>
+      <div>
+        <p className="u-italic">150</p>
+        <button>SUBMIT</button>
+      </div>
+    </form>
+  );
 }
 
 export default FeedbackForm;
